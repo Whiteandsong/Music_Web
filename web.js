9bafaad7f16f8bb2973d9eaafe4c0d917a915089
Whiteandsong <ziyanb@uci.edu>
2024-02-13 22:33:28 -0800

    add clock

--- web.js
+++ web.js
@@ -2,7 +2,6 @@
     "button2": "White Noise",
     "button3": "Natural Sound",
     "button4": "Cosmic Sound",
-    "button5": "Sound",
     "main": "Main",
     "1": "Sound 1",
     "2": "Sound 2",
@@ -65,3 +64,46 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+var timerRunning = false;
+var timerPaused = false;
+var timerInterval;
+var startTime;
+var elapsedTime = 0;
+var lastTime = 0;
+
+function playStopTimer() {
+    var button = document.getElementById("playStopButton");
+    if (!timerRunning) {
+        // Start the timer
+        startTime = new Date().getTime();
+        timerInterval = setInterval(updateTimer, 1000);
+        button.textContent = "Stop";
+        timerRunning = true;
+    } else {
+        // Stop the timer
+        clearInterval(timerInterval);
+        button.textContent = "Continue";
+        timerRunning = false;
+        lastTime = elapsedTime; // Save the current elapsed time
+    }
+}
+
+function restartTimer() {
+    clearInterval(timerInterval);
+    document.getElementById("playStopButton").textContent = "Start";
+    timerRunning = false;
+    timerPaused = false;
+    elapsedTime = 0;
+    lastTime = 0;
+    document.getElementById("timerDisplay").textContent = "0:00:00";
+}
+
+function updateTimer() {
+    var currentTime = new Date().getTime();
+    elapsedTime = currentTime - startTime + lastTime;
+    var hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    var minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+    document.getElementById("timerDisplay").textContent = hours + ":" + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}

8f3286ce1827d01c581d6c75e33af811bde7e14d
Whiteandsong <ziyanb@uci.edu>
2024-03-02 13:41:24 -0800

    split - js

--- web.js
+++ web.js
@@ -10,6 +10,7 @@
 
 };
 
+// 分屏移动
 function toggleSplitInterface() {
     var splitInterface = document.getElementById('split-interface');
     var button1 = document.getElementById('button1');
@@ -65,46 +66,3 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
-
-var timerRunning = false;
-var timerPaused = false;
-var timerInterval;
-var startTime;
-var elapsedTime = 0;
-var lastTime = 0;
-
-function playStopTimer() {
-    var button = document.getElementById("playStopButton");
-    if (!timerRunning) {
-        // Start the timer
-        startTime = new Date().getTime();
-        timerInterval = setInterval(updateTimer, 1000);
-        button.textContent = "Stop";
-        timerRunning = true;
-    } else {
-        // Stop the timer
-        clearInterval(timerInterval);
-        button.textContent = "Continue";
-        timerRunning = false;
-        lastTime = elapsedTime; // Save the current elapsed time
-    }
-}
-
-function restartTimer() {
-    clearInterval(timerInterval);
-    document.getElementById("playStopButton").textContent = "Start";
-    timerRunning = false;
-    timerPaused = false;
-    elapsedTime = 0;
-    lastTime = 0;
-    document.getElementById("timerDisplay").textContent = "0:00:00";
-}
-
-function updateTimer() {
-    var currentTime = new Date().getTime();
-    elapsedTime = currentTime - startTime + lastTime;
-    var hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-    var minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
-    document.getElementById("timerDisplay").textContent = hours + ":" + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-}
